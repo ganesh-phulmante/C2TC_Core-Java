2338dd6a34eb340bcaf95667819ad694563903e8
HP <HP@DESKTOP-JT9BR2U.mshome.net>
2024-10-19 18:06:17 +0530

    Day 1 Programs

---
+++ Day_1/src/com/tnsif/basicprograms/FirstProgram.java
@@ -0,0 +1,12 @@
+package com.tnsif.basicprograms;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		System.out.println("This is my First Program of Java");
+	
+		
+
+	}
+
+}
