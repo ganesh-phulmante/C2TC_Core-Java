63485cc5a6f34b4d85234e5235b5e81875b11fc8
HP <HP@DESKTOP-JT9BR2U>
2024-10-19 19:05:42 +0530

    Day 1 programs

---
+++ Day_1/src/com/tnsif/basicprograms/CharDemo.java
@@ -0,0 +1,15 @@
+package com.tnsif.basicprograms;
+
+public class CharDemo {
+
+	public static void main(String[] args) {
+		
+		int numberOne = 34;
+		int number_one = 56;
+		int Number0 = 58;
+		
+		System.out.println(Number0);
+
+	}
+
+}
--- Day_1/src/com/tnsif/basicprograms/FirstProgram.java
+++ Day_1/src/com/tnsif/basicprograms/FirstProgram.java
@@ -3,9 +3,7 @@ package com.tnsif.basicprograms;
 public class FirstProgram {
 
 	public static void main(String[] args) {
-		System.out.println("This is my First Program of Java");
-	
-		
+		System.out.println("This is first program of java");
 
 	}
 
